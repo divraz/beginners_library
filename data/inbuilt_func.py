9ce4fe37d362cef02a96e788557c2801e8c4ba0d
divyanshuraj6815 <53045449+divyanshuraj6815@users.noreply.github.com>
2020-01-28 17:20:30 +0530

    Update inbuilt_func.py

--- data/inbuilt_func.py
+++ data/inbuilt_func.py
@@ -4,5 +4,5 @@ import tensorflow as tf
 def horizontal_flip (image):
 	return tf.image.random_flip_left_right (image)
 
-def random_crop (image, dimensions)
+def random_crop (image, dimensions):
 	return tf.image.random_crop (image, dimensions)
