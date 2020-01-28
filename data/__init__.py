9080eab0a4140806ad374987b07afdd5ca22d31c
draj <divyanshuraj6815@github.com>
2020-01-28 17:14:12 +0530

    - __init__ file - inbuilt_func

---
+++ data/__init__.py
@@ -0,0 +1,2 @@
+from .random_cutout import random_cutout
+from .inbuilt_func import *
---
+++ data/inbuilt_func.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tensorflow as tf
+
+def horizontal_flip (image):
+	return tf.image.random_flip_left_right (image)
+
+def random_crop (image, dimensions)
+	return tf.image.random_crop (image, dimensions)
